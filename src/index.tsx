4ab81d603c35c905ec02cd12d31e7f4c0b5a280b
butachin <b1016094@fun.ac.jp>
2019-03-26 17:42:50 +0900

    store: roodReducerの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import ReduxLogger from 'redux-logger';
+import { Provider } from 'react-redux';
+import App from './containers/App/App';
+
+const store = createStore(rootReducer, applyMiddleware(ReduxLogger));
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
