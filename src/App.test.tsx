5adb10f984131e6f7d9b3c5f5d132810690dfab4
butachin <b1016094@fun.ac.jp>
2019-03-26 17:41:47 +0900

    test: Appのテストの追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import App from './App';
+import ScreenState, { ScreenType } from './models/states/ScreenState';
+import { FeelingType } from './models/states/FeelingState';
+import { render, cleanup } from 'react-testing-library';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+test('ScreenTypeがINPUT_FEELINGの時、<InputFeeling />が描画される', () => {
+    const inputScreenStateSpy: ScreenState = {
+        screenType: ScreenType.INPUT_FEELING,
+        feelingType: FeelingType.NORMAL
+    };
+    const { getByLabelText } = render(
+        <App screenState={inputScreenStateSpy} />
+    );
+    const inputFeelingScreen = getByLabelText('input-feeling-screen');
+
+    expect(inputFeelingScreen.ELEMENT_NODE).toBe(1);
 });
+
+test('');
+
+// const inputCauseOfFeelingScreen = getByLabelText('input-cause-of-feeling-screen');
