9c2228d55e658097b3a16732552417a5246e1567
butachin <b1016094@fun.ac.jp>
2019-04-11 19:19:48 +0900

    [Chore] InputCauseOfFeeling.testに新たなテストを追加

--- src/components/InputCauseOfFeeling/InputCauseOfFeeling.test.tsx
+++ src/components/InputCauseOfFeeling/InputCauseOfFeeling.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import InputCauseOfFeeling from './InputCauseOfFeeling';
 import { FeelingType } from 'src/models/states/FeelingState';
 
@@ -98,6 +98,86 @@ describe('InputCauseFeeling', () => {
             const sendButton = getByLabelText('send-button');
 
             expect(sendButton.childElementCount).toBe(1);
+        });
+    });
+
+    describe('Integration', () => {
+        // describe('原因ボタンを押した時', () => {
+        //     test('入力された原因がinputCauseOfFeelingsにあるか探しなかった場合追加する', () => {
+        //         const { getByLabelText } = render(
+        //             <InputCauseOfFeeling
+        //                 selectedFeelingState={{
+        //                     feelingId: 2,
+        //                     feelingType: FeelingType.VERY_SATISFIED,
+        //                     name: ''
+        //                 }}
+        //                 causeOfFeelingStates={[]}
+        //                 switchInputFeeling={mockSwitchInputFeeling}
+        //             />
+        //         );
+        //         const amountOfWorkButton = getByLabelText('amount-of-work-button');
+
+        //         fireEvent.click(amountOfWorkButton);
+        //     });
+
+        //     test('入力された原因がinputCauseOfFeelingsにあるか探しあった場合消す', () => {
+        //         const { getByLabelText } = render(
+        //             <InputCauseOfFeeling
+        //                 selectedFeelingState={{
+        //                     feelingId: 2,
+        //                     feelingType: FeelingType.VERY_SATISFIED,
+        //                     name: ''
+        //                 }}
+        //                 causeOfFeelingStates={[]}
+        //                 switchInputFeeling={mockSwitchInputFeeling}
+        //             />
+        //         );
+
+        //     });
+        // });
+
+        describe('Sendボタンのテキスト', () => {
+            test('原因の入力が行われていない時', () => {
+                const { getByLabelText } = render(
+                    <InputCauseOfFeeling
+                        selectedFeelingState={{
+                            feelingId: 2,
+                            feelingType: FeelingType.DISSATISFIED,
+                            name: ''
+                        }}
+                        causeOfFeelingStates={[]}
+                        switchInputFeeling={mockSwitchInputFeeling}
+                    />
+                );
+
+                const sendButton = getByLabelText('send-button');
+
+                expect(sendButton.textContent).toBe('気分だけ伝える');
+            });
+
+            test('原因の入力が行われている時', () => {
+                const { getByLabelText } = render(
+                    <InputCauseOfFeeling
+                        selectedFeelingState={{
+                            feelingId: 2,
+                            feelingType: FeelingType.DISSATISFIED,
+                            name: ''
+                        }}
+                        causeOfFeelingStates={[]}
+                        switchInputFeeling={mockSwitchInputFeeling}
+                    />
+                );
+
+                const amountOfWorkButton = getByLabelText(
+                    'amount-of-work-button'
+                );
+
+                fireEvent.click(amountOfWorkButton);
+
+                const sendButton = getByLabelText('send-button');
+
+                expect(sendButton.textContent).toBe('気分と理由を伝える');
+            });
         });
     });
 });
