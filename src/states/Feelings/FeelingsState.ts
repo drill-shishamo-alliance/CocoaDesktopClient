3636c73d3838b3e634e8ebd1eecd9926f5a632cc
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-06-29 15:44:01 +0900

    [fix] 使わなくなったFeelingTypeを削除

--- src/states/Feelings/FeelingsState.ts
+++ src/states/Feelings/FeelingsState.ts
@@ -1,14 +1,5 @@
 import { Feeling } from 'src/apis/CocoaApi/models/Feelings';
 
-// FeelingState.idと同じ値
-export enum FeelingType {
-  VERY_DISSATISFIED = 1,
-  DISSATISFIED = 2,
-  NORMAL = 3,
-  SATISFIED = 4,
-  VERY_SATISFIED = 5,
-}
-
 type FeelingsState = {
   lists: Feeling[];
   edits: {
