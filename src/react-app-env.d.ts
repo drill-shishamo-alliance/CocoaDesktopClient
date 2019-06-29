8a923214e54c41d72324c98818c192927efd034f
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-06-29 14:03:43 +0900

    [feature] Cocoa APIのIPを環境変数に設定

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+/// <reference types="node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    readonly REACT_APP_COCOA_API_DEV: string;
+  }
+}
