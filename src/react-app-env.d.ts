6f610a6ad99e5a66a5858150e6aec1e4ee0a0e00
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-06-29 14:24:54 +0900

    [feature] 環境変数に簡易認証サーバのIPを追加

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,5 +6,6 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     PUBLIC_URL: string;
     readonly REACT_APP_COCOA_API_DEV: string;
+    readonly REACT_APP_COCOA_SIMPLE_AUTHENTICATE_API: string;
   }
 }
