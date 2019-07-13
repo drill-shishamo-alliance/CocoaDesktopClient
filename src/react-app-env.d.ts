80b42e6cea77d983a2406864b36aaa8d4d66b5c3
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-07-13 10:50:04 +0900

    [chore] SwaggerのIPを環境変数に追加

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,6 +6,7 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     PUBLIC_URL: string;
     readonly REACT_APP_COCOA_API_DEV: string;
+    readonly REACT_APP_COCOA_API_SWAGGER: string;
     readonly REACT_APP_COCOA_SIMPLE_AUTHENTICATE_API: string;
   }
 }
