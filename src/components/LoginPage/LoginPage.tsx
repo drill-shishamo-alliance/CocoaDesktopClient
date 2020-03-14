2dded1ee20f6c6e52a095b59f3586aa6b3531cdc
ShotaroOkada <b1016117@gmail.com>
2020-03-14 16:45:36 +0900

    [ui]ログイン画面のUI整備

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { postLogin } from 'src/actions/User/UserActionCreator';
+import styled from 'styled-components';
 
 const LoginPage: React.FC = () => {
   const [inputTexts, setInputTexts] = useState({
@@ -20,15 +21,61 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
+    <Wrapper>
+      <Title>Cocoa</Title>
       {Object.entries(inputTexts).map(([key, value]) => {
-        return <input type='text' key={key} name={key} value={value} onChange={handleChange} />;
+        return (
+          <Input
+            type='text'
+            key={key}
+            name={key}
+            value={value}
+            placeholder={key}
+            onChange={handleChange}
+          />
+        );
       })}
-      <button key={'loginButton'} onClick={onClick}>
+      <Button key={'loginButton'} onClick={onClick}>
         ログイン
-      </button>
-    </div>
+      </Button>
+    </Wrapper>
   );
 };
 
 export default LoginPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  font-size: 18px;
+  margin-top: 10%;
+  margin-bottom: 24px;
+`;
+
+const Input = styled.input`
+  width: 35%;
+  margin: 0 auto;
+  max-width: 250px;
+  font-size: 12px;
+  padding: 8px;
+  margin-bottom: 8px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+`;
+
+const Button = styled.button`
+  width: auto;
+  margin: auto;
+  font-size: 10px;
+  padding: 8px;
+  margin-top: 8px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+`;
